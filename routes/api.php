67e090748bbd8edb6a3b27b889b1b9669ef8846a
Sandro Haiden <sandrohc2010@hotmail.com>
2019-08-11 18:33:45 -0400

    Adequações para a Hospedagem
    O 000Webhost só suporta os métodos get e post.

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ Route::get('/professores/', 'ProfessorController@lista');
 
 Route::post('/professores/', 'ProfessorController@cadastrar');
 
-Route::delete('/professores/{id}/', 'ProfessorController@deletar');
+Route::get('/professores/{id}/', 'ProfessorController@deletar');
 
-Route::put('/professores/{id}/', 'ProfessorController@atualizar');
+Route::post('/professores/{id}/', 'ProfessorController@atualizar');
 
 //%%%%%%%%%%%%%%%%%%%%%%%%% Curso
 
@@ -28,9 +28,9 @@ Route::get('/curso/', 'CursoController@lista');
 
 Route::post('/curso/', 'CursoController@cadastrar');
 
-Route::delete('/curso/{id}/', 'CursoController@deletar');
+Route::get('/curso/{id}/', 'CursoController@deletar');
 
-Route::put('/curso/{id}/', 'CursoController@atualizar');
+Route::post('/curso/{id}/', 'CursoController@atualizar');
 
 //%%%%%%%%%%%%%%%%%%%%%%%%% ALuno
 
@@ -38,6 +38,6 @@ Route::get('/aluno/', 'AlunoController@lista');
 
 Route::post('/aluno/', 'AlunoController@cadastrar');
 
-Route::delete('/aluno/{id}/', 'AlunoController@deletar');
+Route::get('/aluno/{id}/', 'AlunoController@deletar');
 
-Route::put('/aluno/{id}/', 'AlunoController@atualizar');
+Route::post('/aluno/{id}/', 'AlunoController@atualizar');
